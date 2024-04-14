a764a2cf38c150708064126ad115a3fb5a9ab20a
Your Name <jish261203@gmail.comgit config --global user.name Jishnay>
2024-04-14 20:13:16 +0530

    removed parallax

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,56 +1,57 @@
 /* eslint-disable react/no-unescaped-entities */
-import './about.scss'
+import "./about.scss";
 
 const About = () => {
-const handleResume = () => {
-	const resumeUrl =
-		"https://drive.google.com/file/d/1GK1MG5UrTDnxkQYK4gMYddOrQ_fuv8yV/view?usp=drive_link";
-	try {
-		window.open(resumeUrl, "_blank");
-	} catch (error) {
-		console.error("Error opening resume:", error);
-		alert("Sorry, we couldn't open the resume. Please try again later.");
-	}
+	const handleResume = () => {
+		const resumeUrl =
+			"https://drive.google.com/file/d/1zMSbV53JEtPlkZV9ESC0AKn5v0pGDx6A/view?usp=drive_link";
+		try {
+			window.open(resumeUrl, "_blank");
+		} catch (error) {
+			console.error("Error opening resume:", error);
+			alert("Sorry, we couldn't open the resume. Please try again later.");
+		}
+	};
+
+	return (
+		<section className="about">
+			<h2>About</h2>
+			<div className="content">
+				<img src="/programmer.gif" alt="programmer" className="aboutImage" />
+				<ul className="aboutItems">
+					<li className="aboutList">
+						<img src="/frontend.png" alt="frontend" />
+						<div className="aboutContent">
+							<h3>Frontend Developer</h3>
+							<p>
+								Seasoned frontend developer specializing in crafting responsive,
+								high-performance websites with precision and skill.
+							</p>
+						</div>
+					</li>
+					<li className="aboutList">
+						<img src="/backend.png" alt="backend" />
+						<div>
+							<h3>Backend Developer</h3>
+							<p>
+								Proficient in designing efficient backend systems and APIs for
+								optimal performance.
+							</p>
+						</div>
+					</li>
+					<li className="aboutList">
+						<img src="/applicant.png" alt="applicant" />
+						<div className="aboutContent">
+							<h3>Resume</h3>
+							<button onClick={handleResume} className="resumebtn">
+								Click here
+							</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</section>
+	);
 };
 
-    return (
-			<section className="about">
-				<h2>About</h2>
-				<div className="content">
-					<img src="/programmer.gif" alt="programmer" className="aboutImage" />
-					<ul className="aboutItems">
-						<li className="aboutList">
-							<img src="/frontend.png" alt="frontend" />
-							<div className='aboutContent'>
-								<h3>Frontend Developer</h3>
-								<p>
-									Seasoned frontend developer specializing in crafting
-									responsive, high-performance websites with precision and
-									skill.
-								</p>
-							</div>
-						</li>
-						<li className="aboutList">
-							<img src="/backend.png" alt="backend" />
-							<div>
-								<h3>Backend Developer</h3>
-								<p>
-									Proficient in designing efficient backend systems and APIs for
-									optimal performance.
-								</p>
-							</div>
-						</li>
-						<li className="aboutList">
-							<img src="/applicant.png" alt="applicant" />
-							<div className='aboutContent'>
-								<h3>Resume</h3>
-								<button onClick={handleResume} className='resumebtn'>Click here</button>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</section>
-		);
-}
-
-export default About
+export default About;
